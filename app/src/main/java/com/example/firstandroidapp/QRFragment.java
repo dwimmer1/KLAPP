209482ae9cab4896a340f2393ece306b6187fd65
manue <mrester@htl-steyr.ac.at>
2024-01-12 08:54:41 +0100

    SecondFragment

--- app/src/main/java/com/example/firstandroidapp/QRFragment.java
+++ app/src/main/java/com/example/firstandroidapp/QRFragment.java
@@ -1,21 +1,40 @@
 package com.example.firstandroidapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class QRFragment extends Fragment {
 
-        public QRFragment() {
-            // Erforderlicher leerer öffentlicher Konstruktor
-        }
+    public QRFragment() {
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_q_r, container, false);
-        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_q_r, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button scanButton = view.findViewById(R.id.scan_btn);
+
+        scanButton.setOnClickListener(v -> startQRScan());
+    }
+
+    private void startQRScan() {
+        //new IntentIntegrator(getActivity()).initiateScan();
+        IntentIntegrator integrator = new IntentIntegrator(getActivity());
+        integrator.setPrompt("");
+        integrator.setOrientationLocked(false);
+        integrator.setBeepEnabled(true);
+        integrator.initiateScan();
+    }
 }
