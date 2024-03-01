342dec88295c57881d387962db033024b87c926e
Daniel <dwimmer1@htl-steyr.ac.at>
2024-03-01 16:04:53 +0100

    FragmentList

---
+++ app/src/main/java/com/example/firstandroidapp/KlettersteigListFragment.java
@@ -0,0 +1,109 @@
+package com.example.firstandroidapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firstandroidapp.databinding.FragmentUserdataBinding;
+
+import java.util.List;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+public class KlettersteigListFragment extends Fragment implements DataFetcher.OnDataFetchedListener {
+
+    private FragmentUserdataBinding binding;
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentUserdataBinding.inflate(inflater, container, false);
+
+        DataFetcher dataFetcher = new DataFetcher(this);
+       // dataFetcher.execute("http://192.168.56.1/Klapp/getData.php");
+        dataFetcher.execute("https://klettersteig-app.at/daten/getDataKlapp.php");
+
+
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onDataFetched(List<UserData> userDataList) {
+        // TODO gehört noch gemacht das der benutzer welcher eingeloggt ist angezeigt wird (FINISSHE)
+        String userName = LoggendUserSingleton.getInstance().getUserNames();
+        if (!userDataList.isEmpty()) {
+
+            for (int i = 0; i < userDataList.size(); i++) {
+                UserData firstUser = userDataList.get(i); // Beispiel: Nehme den ersten Benutzer
+
+                if (firstUser.getUsername().contains(userName)){
+
+                    binding.textViewUsername.setText("Username: " + firstUser.getUsername());
+                    binding.textViewEmail.setText("Email: " + firstUser.getEmail());
+                    binding.textViewPassword.setText("Password: " + firstUser.getPassword());
+                    binding.textViewUserId.setText("UserId: " + firstUser.getUserid());
+                    binding.textViewPhoneNumber.setText("Phone Number: " + firstUser.getPhoneNumber());
+                    binding.textViewDateAccountCreated.setText("Account Created: " + firstUser.getDateAccountCreated());
+                    binding.textViewUserRole.setText("UserRole: " + firstUser.getUserRole());
+                }
+            }
+
+
+            //binding.textViewUserRole.setText("RCode: " + firstUser.getQRCode())
+            //binding.textView.
+
+
+        }
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        /*
+        textViewUsername = view.findViewById(R.id.);
+        textViewEmail = view.findViewById(R.id.textViewEmail);
+        //tex = view.findViewById(R.id.textViewPassword);
+        textViewUserId = view.findViewById(R.id.textViewUserId);
+        textViewPhoneNumber = view.findViewById(R.id.textViewPhoneNumber);
+        textViewDateAccountCreated = view.findViewById(R.id.textViewDateAccountCreated);
+        textViewUserRole = view.findViewById(R.id.textViewUserRole);
+        loadUserDataFromDatabase();
+
+        */
+
+        binding.BtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(KlettersteigListFragment.this)
+                        .navigate(R.id.action_ThirdFragment_to_FirstFragment);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
